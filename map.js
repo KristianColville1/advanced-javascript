04b1e87cbb56f85ea80685d1eb7614ac3d46ce03
KristianColville1 <kristiancolville1@gmail.com>
2022-06-04 18:44:15 +0100

    Add map array method examples

--- map.js
+++ map.js
@@ -1,17 +1,32 @@
+//..........................................Using a for loop
+let nums = [ 1, 2, 3, 4 ,5 ];
+let results = [];
+for (let num of nums){
+    results.push(num * 2);
+}
 
-// Using a for loop
-
-
-// Using map()
-
-
-// Simplified w/ map()
-
-
-// Simplfied w/ map() + arrow function
+console.log(results);
 
 
-// With objects:
+//..........................................Using map()
+const multiByTwo = function (num) {
+    return num * 2;
+}
+const mapResults = nums.map(multiByTwo);
+console.log(mapResults);
+
+
+//..........................................Simplified w/ map()
+const simplified = nums.map(function(num) {return num * 2});
+console.log(simplified);
+
+
+//..........................................Simplified w/ map() + arrow function
+const arrowMap = nums.map(num => num * 2);
+console.log(arrowMap);
+
+
+//..........................................With objects:
 const students = [
     {
       id: 1,
@@ -31,4 +46,10 @@ const students = [
       profession: 'Designer',
       skill: 'CSS'
     },
-  ];
+];
+
+// create an array containing an array for each student their id and name;
+
+const studentWithIds = students.map(student => [ student.name, student.id]);
+console.log(studentWithIds);
+
