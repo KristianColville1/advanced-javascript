30b6e882487281feea7f3f5401132d7140f1a056
KristianColville1 <kristiancolville1@gmail.com>
2022-06-04 18:19:13 +0100

    Initial commit

---
+++ map.js
@@ -0,0 +1,34 @@
+
+// Using a for loop
+
+
+// Using map()
+
+
+// Simplified w/ map()
+
+
+// Simplfied w/ map() + arrow function
+
+
+// With objects:
+const students = [
+    {
+      id: 1,
+      name: 'Mark',
+      profession: 'Developer',
+      skill: 'JavaScript'
+    },
+    {
+      id: 2,
+      name: 'Ariel',
+      profession: 'Developer',
+      skill: 'HTML'
+    },
+    {
+      id: 3,
+      name: 'Jason',
+      profession: 'Designer',
+      skill: 'CSS'
+    },
+  ];
